9e5f1937f8b5b2e45ae37ff53c976ebdfd71d5e4
Ido Wigman <IdoWigman@gmail.com>
2026-06-14 23:18:15 +0300

    a

--- src/MyDataStructure.java
+++ src/MyDataStructure.java
@@ -11,6 +11,11 @@ public class MyDataStructure {
      *
      * @param N The maximal number of items that may reside in the DS.
      */
+
+    /*
+    Time complexity: Theta(N) worst case.
+    Instantiating a skip list and a hash table in the size of N each has a runtime of Theta(N).
+     */
     public MyDataStructure(int N) {
         this.skipList = new IndexableSkipList(N);
         HashFactory<Long> hashFactory = new MultiplicativeShiftingHash();
@@ -22,6 +27,13 @@ public class MyDataStructure {
      * In the following functions,
      * you should REMOVE the place-holder return statements.
      */
+
+    /*
+    Time complexity: Theta(log n) expected.
+    The method contains has a runtime of Theta(1).
+    A skip list has an expected runtime of Theta(log n) to insert a value.
+    The reference has Theta(1) runtime to insert it to the hash table.
+     */
     public boolean insert(int value) {
         if (contains(value)) {
             return false;
@@ -30,6 +42,12 @@ public class MyDataStructure {
         hashTable.insert((long) value, newNode);
         return true;
     }
+    /*
+    Time complexity: Theta(log n) expected if the value exists, Theta(1) if doesn't.
+    Search on a hashtable has an expected runtime of Theta(1) (stops here if the value doesn't exist).
+    Using the reference, deleting it from a skip list an expected runtime of Theta(log n).
+    Deleting the value from the hash table has an expected runtime of Theta(1).
+     */
 
     public boolean delete(int value) {
         AbstractSkipList.SkipListNode nodeToDelete = hashTable.search((long) value);
@@ -41,17 +59,38 @@ public class MyDataStructure {
         return true;
     }
 
+    /*
+    Time complexity: Theta(1) expected.
+    A hash table has an expected runtime of Theta(1) to search for a value.
+     */
+
     public boolean contains(int value) {
         return hashTable.search((long) value) != null;
     }
+
+    /*
+    Time complexity: Theta(log n) expected.
+    The Indexable Skip List as an expected runtime of Theta(log n) for the rank method.
+     */
 
     public int rank(int value) {
         return skipList.rank(value);
     }
 
+    /*
+    Time complexity: Theta(log n) expected.
+    The Indexable Skip List as an expected runtime of Theta(log n) for the select method.
+     */
+
     public int select(int index) {
         return skipList.select(index);
     }
+
+    /*
+     Time Complexity: Theta(|L|) Expected
+     Theta(1) lookup to find the 'low' node instantly via Hash Table.
+     Traverses |L| steps forward using Level 0 linked list pointers.
+     */
 
     public List<Integer> range(int low, int high) {
         AbstractSkipList.SkipListNode curr = hashTable.search((long) low);
